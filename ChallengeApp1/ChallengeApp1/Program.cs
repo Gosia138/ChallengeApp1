182b4137a5ed5dba42af812253cb5c6bbb569a16
Gosia138! <molas.malgorzata@gmail.com>
2023-09-19 21:26:54 +0200

    Zadanie dzień 4

--- ChallengeApp1/ChallengeApp1/Program.cs
+++ ChallengeApp1/ChallengeApp1/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string name = "Ewa";
+string gender = "female";
+int age = 35;
+if (name == "Ewa"&& age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+if(gender=="female")
+{
+    if (age<30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta powyżej 30 lat");
+    }
+}
+if (gender != "female")
+{
+    if (age<18)
+    {
+        Console.WriteLine("Niepełnoletni Męczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni Mężczyzna");
+    }
+}
