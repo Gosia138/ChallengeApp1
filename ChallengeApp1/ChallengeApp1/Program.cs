83d1ab36a1560a40bb41395b3738ebe10509dca4
Gosia138! <molas.malgorzata@gmail.com>
2023-09-21 20:56:48 +0200

    Korekta zadania 4

--- ChallengeApp1/ChallengeApp1/Program.cs
+++ ChallengeApp1/ChallengeApp1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 string name = "Ewa";
-string gender = "fema";
-int age = 19;
+string gender = "female";
+int age = 33;
 if (name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa lat 33");
@@ -11,7 +11,12 @@ else if(age<30 && gender=="female")
 
     {
         Console.WriteLine("Kobieta poniżej 30 lat");
+
     }
+else if (age==30 && gender=="female")
+{
+    Console.WriteLine("Kobieta lat 30");
+}
 else if (age>30 && gender=="female")
     {
         Console.WriteLine("Kobieta powyżej 30 lat");
